a9404233cb32b501def9a983b28b4d7b4e1a02ac
Sanjana Bajaj <sanabajaj21@gmail.com>
2026-05-05 21:17:59 -0400

    try classification

--- trees-HW-4.R
+++ trees-HW-4.R
@@ -48,11 +48,56 @@ ems.boost <- gbm(response_time ~ call_final_disposition + city + priority +
                     shrinkage = 0.3 # lambda
 )
 
-## (b)
 pred.boost <- predict(ems.boost, newdata=test, n.trees=2000)
 ems.boost.test.mse <- mean((pred.boost - test$response_time)^2)
 
+## try classification
+# < 10 mins and >= 10 mins
+# 188404 are < 10, 164666 are >= 10, roughly balanced
+# < 10 mins 90% of the time is the SF policy goal
 
+train$response_class <- factor(ifelse(train$response_time < 10, "fast", "slow"))
+test$response_class  <- factor(ifelse(test$response_time < 10, "fast", "slow"))
+
+ems.tree.class <- tree(
+  response_class ~ call_final_disposition + city + priority +
+    number_of_alarms + fire_prevention_district +
+    medical_als + medical_bls + fire_response +
+    command + support + hour + day_of_week,
+  data = train,
+  mindev = 0.001
+)
+
+# prune, get mse
+ems.cv.class <- cv.tree(ems.tree.class, K=5)
+best.size.class <- min(ems.cv.class$size[ems.cv.class$dev == min(ems.cv.class$dev)])
+
+ems.tree.class.pruned <- prune.tree(ems.tree.class, best=best.size.class)
+
+pred.cv.class <- predict(ems.tree.class.pruned, newdata = test, type="class")
+misclass.rate <- mean(pred.cv.class != test$response_class)
+# 19% error rate
+
+train$response_class <- ifelse(train$response_time < 10, 1,0)
+test$response_class  <- ifelse(test$response_time < 10, 1,0)
+
+## try boosting with classification
+ems.boost.class <- gbm(
+  response_class ~ call_final_disposition + city + priority +
+    number_of_alarms + fire_prevention_district +
+    medical_als + medical_bls + fire_response +
+    command + support + hour + day_of_week,
+  data = train,
+  distribution = "bernoulli",
+  n.trees = 2000,
+  interaction.depth = 2,
+  shrinkage = 0.05
+)
+
+pred.prob <- predict(ems.boost.class, newdata = test, n.trees = 2000, type = "response")
+pred.class <- ifelse(pred.prob > 0.5, 1, 0)
+misclass.rate.boost <- mean(pred.class != test$response_class)
+# 18.5% error rate
 
 
 
